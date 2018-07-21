ba21cdce190ca495ec36369d775bb2e03d82742f
navec <navecbatchi@gmail.com>
2018-07-21 15:23:11 +0200

    sample test

---
+++ src/main/java/itec/fcc/rpp/zuul/api/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package itec.fcc.rpp.zuul.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by gael on 21/07/2018.
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
